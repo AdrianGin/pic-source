9b50989e1635f2284caa7647f04757ddc3f6b2e4
Adrian Gin <adrian.gin@gmail.com>
2012-07-18 18:09:59 +1200

    still

--- FuncLib/External/UI_LCD/LCDInterface.c
+++ FuncLib/External/UI_LCD/LCDInterface.c
@@ -35,17 +35,12 @@ void LCDInterface_Print(HD44780lcd_t* lcd, uint8_t* string, uint8_t n)
 void LCD_ClearLine(HD44780lcd_t* lcd, uint8_t row)
 {
     UI_LCD_Pos(lcd, row, 0);
-    myprintf("ROW1:", lcd->RowPos);
     uint8_t i;
     for( i = 0; i <= lcd->MAX_COL; i++ )
     {
         UI_LCD_Char(lcd, ' ');
     }
-
-
     UI_LCD_Pos(lcd, row, 0);
-    myprintf("WTF:", lcd->RSStatus);
-    myprintf("ROW2:", lcd->RowPos);
 }
 
 void LCD_SendChar(HD44780lcd_t* lcd, uint8_t byte)
@@ -57,7 +52,7 @@ void LCD_SendChar(HD44780lcd_t* lcd, uint8_t byte)
         //LCD_ClearLine(lcd, 2);
         //LCD_ClearLine(lcd, 1);
         //LCD_ClearLine(lcd, 0);
-        //UI_LCD_Clear(lcd);
+        UI_LCD_Clear(lcd);
         pendingClear = 0;
     }
 
