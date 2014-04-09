6e50da525950056e5182e896ebb9ca230e97fdc8
Adrian Gin <adrian.gin@gmail.com>
2014-04-09 19:30:41 +0100

    Added STM32 Synth

---
+++ STMF4_Synth/system_stm32f4xx.d
@@ -0,0 +1,8 @@
+.\system_stm32f4xx.o: RTE\Device\STM32F407VG\system_stm32f4xx.c
+.\system_stm32f4xx.o: D:\Keil\ARM\PACK\Keil\STM32F4xx_DFP\1.0.5\Device\Include\stm32f4xx.h
+.\system_stm32f4xx.o: D:\Keil\ARM\PACK\ARM\CMSIS\3.20.3\CMSIS\Include\core_cm4.h
+.\system_stm32f4xx.o: D:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\system_stm32f4xx.o: D:\Keil\ARM\PACK\ARM\CMSIS\3.20.3\CMSIS\Include\core_cmInstr.h
+.\system_stm32f4xx.o: D:\Keil\ARM\PACK\ARM\CMSIS\3.20.3\CMSIS\Include\core_cmFunc.h
+.\system_stm32f4xx.o: D:\Keil\ARM\PACK\ARM\CMSIS\3.20.3\CMSIS\Include\core_cm4_simd.h
+.\system_stm32f4xx.o: D:\Keil\ARM\PACK\Keil\STM32F4xx_DFP\1.0.5\Device\Include\system_stm32f4xx.h
