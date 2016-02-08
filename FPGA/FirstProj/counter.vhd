dd89d89864060bededdfd4473118d8059a54caf0
Adrian Gin <adrian.gin@gmail.com>
2016-02-08 15:49:20 +1300

    Added UART IP block.

--- FPGA/FirstProj/counter.vhd
+++ FPGA/FirstProj/counter.vhd
@@ -5,23 +5,29 @@ use IEEE.numeric_std.ALL;
 ENTITY counter IS
 GENERIC ( bit_depth : INTEGER := 8;
 			 init_val  : INTEGER := 00000);
-PORT ( clk : IN STD_LOGIC ;
+PORT ( e 	: IN STD_LOGIC ;
+		 clr 	: IN STD_LOGIC;
+		 clk 	: IN STD_LOGIC ;
 		 qout : OUT STD_LOGIC_VECTOR(bit_depth-1 downto 0)
 		 );
 
 END;
 
-ARCHITECTURE counter_8 OF counter IS
+ARCHITECTURE behaviour OF counter IS
 	SIGNAL   cnt: UNSIGNED(bit_depth-1 downto 0) := to_unsigned(init_val, bit_depth);
 
 BEGIN
-	PROCESS(clk)
+	PROCESS(clk, clr)
 		BEGIN
-			IF clk'EVENT AND clk = '1' THEN
-				cnt <= cnt + 1;
+			IF clk'EVENT AND clk = '1' AND clr = '1' THEN
+				cnt <= (OTHERS => '0');
+			ELSIF clk'EVENT AND clk = '1' AND e = '1' AND clr = '0' THEN
+				cnt <= cnt + 1;				
 			END IF;
+				
 	END PROCESS;
+
 	
 	qout <=  STD_LOGIC_VECTOR(cnt);
 	
-END counter_8;
+END behaviour;
--- FPGA/FirstProj/timer.vhd
+++ FPGA/FirstProj/timer.vhd
@@ -6,6 +6,7 @@ ENTITY timer IS
 
 PORT ( clk : IN STD_LOGIC;
 		 div : IN STD_LOGIC_VECTOR(15 downto 0);
+		 rst : IN STD_LOGIC;
 		 clkOut : OUT STD_LOGIC);
 		 
 END;
@@ -13,13 +14,16 @@ END;
 
 ARCHITECTURE behaviour OF timer IS
 	SIGNAL cmp : UNSIGNED(15 downto 0) := X"0000";
-	SIGNAL usdiv : UNSIGNED(15 downto 0) := X"0000";
+	SIGNAL usdiv : UNSIGNED(15 downto 0) := X"0001";
 	SIGNAL outBuf : STD_LOGIC := '0';
 BEGIN
 	
-	PROCESS(clk, div, cmp)
+	PROCESS(rst, clk, div, cmp)
 	BEGIN
-		IF clk'EVENT AND clk = '1' THEN
+	
+		IF clk'EVENT AND clk = '1' AND rst = '1' THEN
+			cmp <= X"0000";
+		ELSIF clk'EVENT AND clk = '1' AND rst = '0' THEN
 			usdiv <= unsigned(div) - 1;
 			
 			IF cmp = usdiv THEN
@@ -28,9 +32,8 @@ BEGIN
 			ELSE
 				cmp <= cmp + 1;
 			END IF;
-			
-
 		END IF;
+		
 	END PROCESS;
 	
 	clkOut <= outBuf;
